5a8eef3ff1093774375ff0984ffd48d1c797ecdb
MaykonRO <maykonro2004@gmail.com>
2026-01-11 22:58:58 -0300

    Atualização do controller para permitir update
    com essa atualização o controller vai permitir fazer os updates no postman
    
    testei todos os 3 metodos e estao funcionando
    
    proximos passos:
    validações

--- api/src/main/kotlin/com/biblioteca/api/controller/BookController.kt
+++ api/src/main/kotlin/com/biblioteca/api/controller/BookController.kt
@@ -2,9 +2,12 @@ package com.biblioteca.api.controller
 
 import com.biblioteca.api.dto.CreateBookRequest
 import com.biblioteca.api.dto.CreateBookResponse
+import com.biblioteca.api.dto.UpdateBookRequest
 import com.biblioteca.api.model.Book
 import com.biblioteca.api.service.BookService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -23,5 +26,10 @@ class BookController(
     @PostMapping
     fun create(@RequestBody request: CreateBookRequest): CreateBookResponse {
         return bookService.create(request)
+    }
+
+    @PatchMapping("/{id}/status")
+    fun updateBook(@PathVariable id: Long, @RequestBody request: UpdateBookRequest): CreateBookResponse{
+        return bookService.updateStatus(id,request)
     }
 }
